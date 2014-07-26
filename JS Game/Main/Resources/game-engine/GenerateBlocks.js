a0057cd69e1287579125b388cc04a372b517dd90
AKJ1 <kellerr@abv.bg>
2014-07-26 18:15:33 +0300

    Added block types and drawing, broke input text

--- JS Game/Main/Resources/game-engine/GenerateBlocks.js
+++ JS Game/Main/Resources/game-engine/GenerateBlocks.js
@@ -1,4 +1,5 @@
-var reader = new XMLHttpRequest() || new ActiveXObject('MSXML2.XMLHTTP');
+ 
+var reader = new XMLHttpRequest();
 
 function loadFile() {
     reader.open('get', 'Resources/game-engine/test.txt', true);
@@ -8,37 +9,42 @@ function loadFile() {
 function generateBlocks() {
     var c = document.getElementById("field");
     var ctx = c.getContext("2d");
-
-    if(reader.readyState==4) {
-
+    var blox = [];
         var field = reader.responseText;
-
+        if (field == ""){
+            field = "\n nnn nnn nnn nnn\n  t  t   t    t \n  d  ddd ddd  d \n  t  t     t  t \n  n  nnn nnn  n ";
+        }
         var width = 0;
         var height = 0;
         for(letter in field) {
             if (field[letter] == "n") {
-                blocks.push(new Block("n", width, height));
+                blox.push(new Block("n", width, height,c));
+                width+= c.width/18;
             }
             else if (field[letter] == "p"){ //PowerUp
-                blocks.push(new Block("p", width, height));
+                blox.push(new Block("p", width, height,c));
+                width+= c.width/18;
             }
             else if (field[letter] == "d"){ //Double
-                blocks.push(new Block("d", width, height));
+                blox.push(new Block("d", width, height,c));
+                width+= c.width/18;
             }
             else if (field[letter] == "t"){ //Triple
-                blocks.push(new Block("t", width, height));
+                blox.push(new Block("t", width, height,c));
+
+                width+= c.width/18;
             }
             else if (field[letter] == '\u0020') {
                 //Current char is a blank space
-                width+= 51;
+                width+= c.width/18;
             }
             else if (field[letter] == '\u000A') {
                 //Current char is a new line
                 width = 0;
-                height += 25;
+                height += c.height/24;
             }
-        }
         //\u0020
         //\u000A
     }
+    return blox;
 }
---
+++ JS Game/test.js
@@ -0,0 +1,250 @@
+Math.signum = function(arg) {
+    if(arg > 0){
+        return 1;
+    } else if(arg < 0){
+        return -1;
+    } else if(arg === 0){
+        return 0;
+    }
+
+};
+
+window.onload = function () {
+    loadFile();
+    var theCanvas = document.getElementById('field');
+    var canvasCtx = theCanvas.getContext('2d');
+
+    canvasCtx.fillStyle = 'red';
+    canvasCtx.strokeStyle = 'black';
+
+    function Envelope(x, y, width, height, lives) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+        this.moveSpeed = 0;
+        this.currentSpeed = 12; //some workaround for smooth animation with some initial value
+        this.sticky = true;
+        this.lives = lives;
+        this.image; //when we include graphix
+
+        this.draw = function (canvasCtx) {
+            canvasCtx.beginPath();
+            canvasCtx.moveTo(this.x, this.y);
+            canvasCtx.fillStyle = "#FF0000";
+            canvasCtx.rect(this.x, this.y, this.width, this.height);
+            canvasCtx.fill();
+            canvasCtx.lineWidth = 2;
+            canvasCtx.stroke();
+        };
+
+        this.move = function () {
+            //envelope border collision check
+            if (this.x <= 0) {
+                this.x = 0;
+            } else if (this.x + this.width >= theCanvas.width) {
+                this.x = theCanvas.width - this.width;
+            }
+
+            this.x += this.moveSpeed;
+        };
+
+    }
+    function Block(kind, x, y){
+        this.init = function() {
+            this.height = canvasCtx.canvas.clientHeight/20;
+            this.width = canvasCtx.canvas.clientWidth/18;
+            switch(kind){
+                case "n":{ //Normal block
+                    this.health = 1;  // Hits Required for kill
+                    this.fillColor = "#000000"; //Color
+                    this.hittable = true; // 
+                    this.powerUP = 0.01;
+                    break;
+                }
+                case "p":{ // PowerUP dropper
+                    this.health = 1;
+                    this.hittable = true;
+                    this.fillColor = "#00ff99";
+                    this.powerUP = 1;
+                    break;
+                }
+                case "d":{ // Double hit block
+                    this.health = 2;
+                    this.hittable = true;
+                    this.fillColor = "#FADE00";
+                    this.powerUP = 0.02;
+                    break;
+                }
+                case "t":{ // Triple hit block
+                    this.health = 3;
+                    this.hittable = true;
+                    this.powerUP = 0.05;
+                    this.fillColor = "#DA9900";
+                    break;
+                }
+            }
+            this.draw = function(canvasCtx) {
+
+                canvasCtx.fillStyle = this.fillColor;
+                canvasCtx.rect(this.x, this.y, this.width, this.height);
+                canvasCtx.fill();
+            };
+        };
+    }
+    function Ball(cX, cY, rad, moveSpeed, directionX, directionY) {
+        this.cX = cX;
+        this.cY = cY;
+        this.rad = rad;
+        this.mainSpeed = moveSpeed;
+        this.moveSpeedX = moveSpeed;
+        this.moveSpeedY = moveSpeed;
+        this.directionX = directionX;
+        this.directionY = directionY;
+
+        this.draw = function (canvasCtx) {
+            canvasCtx.beginPath();
+            canvasCtx.arc(this.cX, this.cY, this.rad, 0, 2 * Math.PI); // Makes Arc of 2*pi = Circle
+            canvasCtx.fill(); // Fills it
+            canvasCtx.stroke(); // Adds the black outline
+        };
+
+        this.move = function () {
+            this.cX += this.moveSpeedX; // removed switch, instead just set movespeed variable to negative.
+            this.cY += this.moveSpeedY; // removed switch, instead just set movespeed variable to negative.
+            this.checkCollision(); // checks if the ball collides with anything
+        };
+
+        this.checkCollision = function () {
+            this.rightBorder = this.cX + this.rad; 
+            this.leftBorder = this.cX - this.rad;
+            this.topBorder = this.cY - this.rad;
+            this.bottomBorder = this.cY + this.rad;
+
+            //playfield border and envelope collision checks
+            if (this.leftBorder <= 0) {
+                this.moveSpeedX = -this.moveSpeedX; // sets variable to move right
+            } else if (this.rightBorder >= theCanvas.width) {
+                this.moveSpeedX = -this.moveSpeedX; //sets variable to move left
+            }
+            /*Bug : Sometimes the ball goes too fast , we should probably set a limit of atleast MS = 2 for X and Y.*/
+            /*Bug : Somethimes the ball can go fast enough as to exit the boundaries of the level, leaving it glitched*/
+            if (this.topBorder <= 0) {
+                this.moveSpeedY = -this.moveSpeedY; // sets variable to move down 
+            } else if (this.cX >= player.x &&
+                this.cX <= (player.x + player.width) &&
+                this.bottomBorder >= player.y &&
+                this.bottomBorder <= player.y + player.height) {
+                    if (player.sticky) {
+                        if (player.x >= 0 && player.x + player.width <= theCanvas.width) {
+                            this.moveSpeedX = player.moveSpeed;
+                            this.moveSpeedY = 0;
+                        }else{
+                            this.moveSpeedX = 0;
+                            this.moveSpeedY = 0;
+                        }
+                    }else{
+                        this.moveSpeedX = this.mainSpeed *-2*(1 - ((this.cX-player.x)/(player.width/2))); //The values for X and Y movespeed are reciproc and equal 2* movespeed. This chooses direction dynamically.
+                        this.moveSpeedY = -this.mainSpeed *2*(1 - Math.abs(1 - ((this.cX-player.x)/(player.width/2))));// player movement
+                        if (Math.abs(this.moveSpeedX) > this.mainSpeed*1.5) {
+                            this.moveSpeedX = this.mainSpeed*1.5*Math.signum(this.moveSpeedX);
+                            this.moveSpeedY = this.mainSpeed*0.5*Math.signum(this.moveSpeedY);
+                        }
+                    }
+            }
+            else if (this.bottomBorder >= theCanvas.height) {
+                if (balls.length > 1) {
+                    var newBalls = [];
+                    for(var i = 0; i < balls.length; i++){
+                        if(balls[i].bottomBorder <= theCanvas.height){
+                            newBalls.push(balls[i]);
+                        }
+                    }
+                    balls = newBalls;
+                }else{
+                    player.lives -= 1;  
+                    player.sticky = true;
+                    balls.push(new Ball(player.x + (player.width / 2), (player.y - 7), 7, 6, 'left', 'up')); // Replaces ball that spawns at player location when it's destroyed.
+                }
+            }
+        };
+
+    }
+
+    //initialize player envelope and ball
+
+    var balls = [];
+    var blocks = [];
+    var player = new Envelope(300, 400, 150, 20, 3);
+    balls.push(new Ball(player.x + (player.width / 2), (player.y - 7), 7, 6, 'left', 'up'));
+
+
+
+
+    //Player Controls Block
+    document.body.addEventListener('keydown', function (e) {
+        if (!e) {
+            e = window.event;
+        }
+
+        switch (e.keyCode) {
+            case 37: // Left
+                player.moveSpeed = (player.currentSpeed * -1); //Sets Player Speed
+                break;
+            case 39: // Right
+                player.moveSpeed = player.currentSpeed; //Sets Player Speed
+                break;
+            case 32, 38:/*space*/ {
+                if (player.sticky) {
+                    player.sticky = 0;
+                    for(var b in balls){
+                        if (b.moveSpeedX == 0 && b.moveSpeedY == 0) {
+                            b.moveSpeedY = -6;
+                        }
+                    }
+                }
+                
+                break;
+            }
+        }
+
+    });
+
+    document.body.addEventListener('keyup', function (e) {
+        if (!e) {
+            e = window.event;
+        }
+
+        switch (e.keyCode) {
+            case 37:
+                if (player.moveSpeed === (player.currentSpeed * -1)) { // Checks if the correct key is pressed. Else Has issues when player button mashes.
+                    player.moveSpeed = 0;
+                }
+                break;
+            case 39:
+                if (player.moveSpeed === player.currentSpeed) { // Checks if the correct key is pressed. Else Has issues when player button mashes.
+                    player.moveSpeed = 0;
+                }
+                break;
+        }
+    });
+
+    //Game Loop
+    function startGame() {
+        canvasCtx.clearRect(0, 0, theCanvas.width, theCanvas.height);
+
+        player.draw(canvasCtx);
+        player.move();
+
+        for (var i = 0; i < balls.length ; i++){
+            balls[i].draw(canvasCtx);
+            balls[i].move(canvasCtx);
+        }
+        for (var i = 0; i < blocks.length; i++){
+            blocks[i].draw();
+        }
+        requestAnimationFrame(startGame);
+        generateBlocks();
+    }
+    requestAnimationFrame(startGame);
+};
